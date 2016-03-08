6cd07715ee1a7e5e0fc7fe6d5cdeed6de8fef798
shayanchabok007 <shayanchabok5@gmail.com>
2016-03-08 17:23:39 +0430

    Create fosh_lock.lua

---
+++ plugins/fosh_lock.lua
@@ -0,0 +1,22 @@
+local function run(msg)
+    if msg.to.type == 'chat' and not is_momod(msg) then
+        chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+        return 'استفاده از کلمات بد ممنوع است'
+    end
+end
+
+return {
+    patterns = {
+    "[Kk][Ii][Rr]",
+    "[Kk][Oo][Ss]",
+    "[Nn][Aa][Nn][Aa][Tt]",
+    "کص",
+    "ننه جنده",
+    "کیر",
+    "کون",
+    "کیرم دهنت",
+    "ننت"
+    }, 
+run = run
+}
+
